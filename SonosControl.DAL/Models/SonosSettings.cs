06333d5d5a299d46a324af19825bf42664314301
Darkatek7 <heeltobias@gmail.om>
2022-12-06 15:48:37 +0100

    Fix Issues when there is no config

--- SonosControl.DAL/Models/SonosSettings.cs
+++ SonosControl.DAL/Models/SonosSettings.cs
@@ -7,6 +7,6 @@ namespace SonosControl.DAL.Models
         public int Volume { get; set; } = 10;
         public TimeOnly StartTime { get; set; } = new TimeOnly(6, 0);
         public TimeOnly StopTime { get; set; } = new TimeOnly(18, 0);
-        public string IP_Adress { get; set; } = "10.0.0.1";
+        public string IP_Adress { get; set; } = "10.0.0.0";
     }
 }
