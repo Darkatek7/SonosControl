8d3d06a2775c96463626a339c5ab8b6ed1a03227
Darkatek7 <50767771+Darkatek7@users.noreply.github.com>
2025-02-10 16:39:19 +0100

    Update SonosConnectorRepo.cs

--- SonosControl.DAL/Repos/SonosConnectorRepo.cs
+++ SonosControl.DAL/Repos/SonosConnectorRepo.cs
@@ -1,6 +1,11 @@
-﻿using ByteDev.Sonos;
+using System.Text;
+using ByteDev.Sonos;
 using ByteDev.Sonos.Models;
+using ByteDev.Sonos.Upnp;
 using SonosControl.DAL.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SonosControl.DAL.Repos
 {
@@ -51,5 +56,45 @@ namespace SonosControl.DAL.Repos
             SonosVolume sonosVolume = new SonosVolume(volume);
             await controller.SetVolumeAsync(sonosVolume);
         }
+
+        private static readonly HttpClient HttpClient = new();
+
+        public async Task SetTuneInStationAsync(string ip, string stationUri)
+        {
+            string soapRequest = $@"
+            <s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/""
+                         s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">
+              <s:Body>
+                <u:SetAVTransportURI xmlns:u=""urn:schemas-upnp-org:service:AVTransport:1"">
+                  <InstanceID>0</InstanceID>
+                  <CurrentURI>x-rincon-mp3radio://{stationUri}</CurrentURI>
+                  <CurrentURIMetaData></CurrentURIMetaData>
+                </u:SetAVTransportURI>
+              </s:Body>
+            </s:Envelope>";
+
+            var content = new StringContent(soapRequest, Encoding.UTF8, "text/xml");
+            string url = $"http://{ip}:1400/MediaRenderer/AVTransport/Control";
+            content.Headers.Clear();
+            content.Headers.Add("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI\"");
+
+            try
+            {
+                var response = await HttpClient.PostAsync(url, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Station set successfully!");
+                }
+                else
+                {
+                    Console.WriteLine($"Error setting station: {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception setting TuneIn station: {ex.Message}");
+            }
+        }
+
     }
 }
