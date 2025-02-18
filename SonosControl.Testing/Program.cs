a79450d87fc451142b9e7d914381bb235e050391
Heel Tobias <theel@collini.eu>
2025-02-18 15:46:13 +0100

    add changes

--- SonosControl.Testing/Program.cs
+++ SonosControl.Testing/Program.cs
@@ -1,22 +1,54 @@
-﻿var startTime = new TimeOnly(6, 0);
-var stopTime = new TimeOnly(17, 0);
-var timeNow = new TimeOnly(17, 1);
+﻿//var startTime = new TimeOnly(6, 0);
+//var stopTime = new TimeOnly(17, 0);
+//var timeNow = new TimeOnly(17, 1);
+//
+//var timeDifference = startTime - timeNow;
+//
+//if (timeNow >= stopTime || startTime >= timeNow)
+//{
+//    var delayInMs = (int)timeDifference.TotalMilliseconds;
+//
+//    Console.WriteLine("Starting in " + delayInMs + " ms.");
+//    Task.Delay(delayInMs).Wait();
+//    Console.WriteLine("Started Playing");
+//}
+//else if (startTime >= timeNow)
+//{
+//    var delayInMs = (int)timeDifference.TotalMilliseconds;
+//
+//    Console.WriteLine("Starting in " + delayInMs + " ms.");
+//    Task.Delay(delayInMs).Wait();
+//    Console.WriteLine("Started Playing");
+//}
+using System.Runtime;
+using ByteDev.Sonos;
+using ByteDev.Sonos.Device;
+using ByteDev.Sonos.Models;
+using ByteDev.Sonos.Upnp.Services.Models;
+using SonosControl.DAL.Interfaces;
+using SonosControl.DAL.Repos;
+SonosDevice test = new SonosDevice("10.1.12.251");
 
-var timeDifference = startTime - timeNow;
+string ip = "10.1.12.251";
 
-if (timeNow >= stopTime || startTime >= timeNow)
-{
-    var delayInMs = (int)timeDifference.TotalMilliseconds;
+Console.WriteLine(test.HardwareVersion);
+Console.WriteLine(test.Udn);
+Console.WriteLine(test.SoftwareVersion);
+Console.WriteLine(test.IpAddress);
+Console.WriteLine(test.ModelName);
+Console.WriteLine(test.ModelDescription);
+Console.WriteLine(test.FriendlyName);
+Console.WriteLine(test.SoftwareVersion);
 
-    Console.WriteLine("Starting in " + delayInMs + " ms.");
-    Task.Delay(delayInMs).Wait();
-    Console.WriteLine("Started Playing");
-}
-else if (startTime >= timeNow)
-{
-    var delayInMs = (int)timeDifference.TotalMilliseconds;
 
-    Console.WriteLine("Starting in " + delayInMs + " ms.");
-    Task.Delay(delayInMs).Wait();
-    Console.WriteLine("Started Playing");
-}
+SonosController controller = new SonosControllerFactory().Create("10.1.12.251");
+
+
+controller.PauseAsync().Wait();
+
+UnitOfWork _uow = new();
+
+//await _uow.ISonosConnectorRepo.SetTuneInStationAsync("10.1.12.251", "stream.rockantenne.bayern/80er-rock/stream/mp3");
+//await _uow.ISonosConnectorRepo.SetTuneInStationAsync("10.1.12.251", "web.radio.antennevorarlberg.at/av-live/stream/mp3");
+
+await _uow.ISonosConnectorRepo.StartPlaying(ip);
