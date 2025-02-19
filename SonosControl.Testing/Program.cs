532f8ea7c853cafe8582cb4e607bb91cb7687bdc
Heel Tobias <theel@collini.eu>
2025-02-19 14:17:10 +0100

    Add Spotify UI

--- SonosControl.Testing/Program.cs
+++ SonosControl.Testing/Program.cs
@@ -44,11 +44,14 @@ Console.WriteLine(test.SoftwareVersion);
 SonosController controller = new SonosControllerFactory().Create("10.1.12.251");
 
 
-controller.PauseAsync().Wait();
+//controller.PauseAsync().Wait();
 
 UnitOfWork _uow = new();
 
 //await _uow.ISonosConnectorRepo.SetTuneInStationAsync("10.1.12.251", "stream.rockantenne.bayern/80er-rock/stream/mp3");
 //await _uow.ISonosConnectorRepo.SetTuneInStationAsync("10.1.12.251", "web.radio.antennevorarlberg.at/av-live/stream/mp3");
+
+//await _uow.ISonosConnectorRepo.StopPlaying("10.1.12.251");
+await _uow.ISonosConnectorRepo.PlaySpotifyTrackAsync("10.1.12.251", "https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF");
 
 await _uow.ISonosConnectorRepo.StartPlaying(ip);
